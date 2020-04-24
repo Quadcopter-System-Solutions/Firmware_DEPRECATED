8b1535761635d91b3bf90c4dfa5b77d893031865
JuliusC4es4r <47398230+JuliusC4es4r@users.noreply.github.com>
2020-04-23 21:59:31 -0700

    Working Copy of voltageRead function

--- voltageRead/voltageRead.ino
+++ voltageRead/voltageRead.ino
@@ -1,5 +1,5 @@
 /*
-    QS Solutions voltageRead.c
+    QS Solutions voltageRead.ino
 
     Designed to read the voltage of each cell in a 3 cell LiPo battery using an Arduino Nano's Analog Inputs.
 
@@ -17,21 +17,52 @@ float voltageOne = 0;  // variable to store the voltage at cell 1
 float voltageTwo = 0; // variable to store the voltage at cell 2
 float voltageThree = 0; // variable to store the voltage at cell 3
 
+float cellOneRatio = 1; // voltage divider ratio of cell one to convert the voltage read to the actual cell voltage
+float cellTwoRatio = 1030.0/470.0; // voltage divider ratio of cell two to convert the voltage read to the actual cell voltage
+float cellThreeRatio = 890.0/330.0; // voltage divider ratio of cell three to convert the voltage read to the actual cell voltage
+
 float referenceVoltage = 4.68; //this value is dependent on the Arduino Nano's internal reference voltage and can be measured with a multimeter between ground and the Arduino's 5V output (the default reference voltage)
+float ADCconversion = referenceVoltage/1023.0; //
 
 void setup() {
   // declare the ledPin as an OUTPUT:
+  pinMode(cellOne, INPUT);
   pinMode(cellTwo, INPUT);
+  pinMode(cellThree, INPUT);
   Serial.begin(9600);
-  Serial.print("Begin: \n");
+  Serial.print("Begin Sketch: \n");
 }
 
 void loop() {
   // read the value from the sensor:
+  delay(1000);
+  
+  sensorValue = analogRead(cellOne);
+  voltageOne =  sensorValue * ADCconversion * cellOneRatio;
+  
   delay(500);
+  
   sensorValue = analogRead(cellTwo);
-  voltageTwo =  sensorValue * (referenceVoltage /1023.0);
-  Serial.print("The voltage is:");
-  Serial.println(voltageTwo);
+  voltageTwo = sensorValue * ADCconversion * cellTwoRatio;
+  
   delay(500);
+  
+  sensorValue = analogRead(cellThree);
+  voltageThree = sensorValue * ADCconversion * cellThreeRatio;
+  
+  delay(500);
+  
+  Serial.print("The voltage in Cell 1 is ");
+  Serial.println(voltageOne);
+  Serial.print("\n");
+
+  Serial.print("The voltage in Cell 2 is ");
+  Serial.println(voltageTwo);
+  Serial.print("\n");
+  
+  Serial.print("The voltage in Cells 3 is ");
+  Serial.println(voltageThree);
+  Serial.print("\n");
+  
+  delay(1000);
 }
